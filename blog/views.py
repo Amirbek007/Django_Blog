9c4638656f3c2b0a7290aa60221d4a15efd50d94
Amirbek007 <95286258+Amirbek007@users.noreply.github.com>
2021-12-07 17:11:51 +0500

    yangi qism

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+def home():
+    return HttpResponse("<h1>Blog home</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Blog About</h1>")
+
