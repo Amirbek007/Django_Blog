608560de68cbebb83a378981b8b8847834ad2823
Amirbek007 <95286258+Amirbek007@users.noreply.github.com>
2021-12-14 16:30:11 +0500

    Oxirgi Ozgarish, css fayl qo'shdik

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
@@ -11,17 +11,26 @@ from django.shortcuts import render
 #def about(request):
 #    return HttpResponse("<h1>Blog About</h1>")
 
-posts = [
-    {
-        'author':'Amirbek',
-        'title':'Blog post 1',
-        'content':'Birinchi postning matni',
-        'date_posted':'Dekabr 9, 2021'
-    },
-]
+#posts = [
+ #   {
+ #       'author':'Amirbek',
+  #      'title':'Blog post 1',
+ #       'content':'Birinchi postning matni',
+ #       'date_posted':'Dekabr 9, 2021'
+ #   },
+ #   
+ #    {
+ #       'author':'Amirbek',
+ #       'title':'Blog post 2',
+ #       'content':'Birinchi postning matni',
+ #       'date_posted':'Dekabr 11, 2021'
+ #   },
+
 
 def home(request):
-    context = {'posts':posts}
+    context = {'posts':Post.objects.all()}
     return render(request, 'blog/home.html', context)
+
+
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
