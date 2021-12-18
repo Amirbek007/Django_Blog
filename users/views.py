70d67bdffaff2daea07ba0ee619801b5f0d1cf3c
Amirbek007 <95286258+Amirbek007@users.noreply.github.com>
2021-12-18 18:12:19 +0500

    Qo'shimcha o'zgarishlar

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.contrib import messages
@@ -12,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form. cleaned_data.get['username']
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'yangi akkount yaratildi. Siz login qila olasiz')
+            return redirect('login')
     else:
         form=UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})    
